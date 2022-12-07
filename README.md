41b333361bd2b4bf42a78a82123d35b9709fbfc9
Abhirup Bakshi <abhirup.bakshi@disroot.org>
2022-12-07 17:16:26 +0530

    created prime.js file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # git_practice_assignmnet_1
+masaischool-assignment-7-12-2022
---
+++ prime.js
@@ -0,0 +1,17 @@
+function checkPrime(num)
+{
+    if(num <= 1)
+    {
+        return false;
+    }
+    
+    for(let i = 2; i <= num**0.5; i++)
+    {
+        if(num % i == 0)
+            return false;
+    }
+    
+    return true;
+}
+
+// console.log(checkPrime(13));
